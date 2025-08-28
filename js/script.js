075ee6290dbbf32dfb32ec6ba0b084fee105cd2c
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-08-28 15:32:55 -0600

    Create project directory

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log("Hi, Mom!");
+
+//create player         .player
+    //name and symbol   .name
+    //                  .symbol
+
+//gameboard                         .board
+    //play a symbol on a cell       .mark
+    //reset (clear)                 .reset
+    //return current arrangement    .status
+
+//game controller   .game
+    //start         .start
+    //player move   .move
+    //switch player .switch
+    //check win     .check
+
+//display controller .display
+    //initialize     .init
+    //begin game     .begin
+    //redraw         .redraw
+    //display winner .winner
